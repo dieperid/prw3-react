28e0933ada20d413cea0f68de8cfd122b8029057
ddieperi <david.dieperink@ik.me>
2026-02-08 11:24:58 +0100

    feat: refactored courseinfo to match exercice 1.1

---
+++ part1/courseinfo/src/components/Part.jsx
@@ -0,0 +1,14 @@
+const Part = (props) => {
+  const name = props.part.name;
+  const exercises = props.part.exercises;
+
+  return (
+    <div>
+      <p>
+        {name} {exercises}
+      </p>
+    </div>
+  );
+};
+
+export default Part;
