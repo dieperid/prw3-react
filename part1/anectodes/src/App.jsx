4ec755c679097180b2e71e4155c425d8654b7226
ddieperi <david.dieperink@ik.me>
2026-02-08 12:13:54 +0100

    feat: added last exercice of part 1, anectodes

--- part1/anectodes/src/App.jsx
+++ part1/anectodes/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from "./components/Header";
+import Anecdote from "./components/Anectode";
+import Winner from "./components/Winner";
+import Button from "./components/Button";
+
+import anecdotes from "./lib/data";
+
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const [allVotes, setAllVotes] = useState(Array(6).fill(0));
+
+  const handleVoteClick = () => {
+    const newAllVotes = [...allVotes];
+    newAllVotes[selected] += 1;
+    setAllVotes(newAllVotes);
+  };
+
+  const handleAnecdoteClick = () => {
+    const arrayIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(arrayIndex);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header name="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votesCount={allVotes[selected]} />
+      <Button onClick={handleVoteClick} text="vote" />
+      <Button onClick={handleAnecdoteClick} text="Next anecdote" />
 
-export default App
+      <Header name="Anecdote with most votes" />
+      <Winner anecdotes={anecdotes} allVotes={allVotes} />
+    </div>
+  );
+};
+
+export default App;
