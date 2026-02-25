62afe05e83f5ac460198bfd66f58344005c9b684
ddieperi <david.dieperink@ik.me>
2026-02-25 09:30:37 +0100

    feat: removed cors

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require("cors");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -18,7 +17,6 @@ const unknownEndpoint = (request, response) => {
 app.use(express.json());
 app.use(requestLogger);
 app.use(morgan("tiny"));
-app.use(cors());
 app.use(express.static("dist"));
 
 let persons = [
